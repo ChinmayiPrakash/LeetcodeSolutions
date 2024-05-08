28d92b089a954dd2fce9f4fa4823a540bf6072b0
ChinmayiPrakash <83453211+ChinmayiPrakash@users.noreply.github.com>
2024-05-08 14:52:50 -0400

    Time: 8 ms (29.78%), Space: 53.3 MB (89.99%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,18 +1,25 @@
+//using Cyclic sort in this code.
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
+        //sort elements based on index such that nums[i] == i+1
        nums = func_disappearedElements(nums);
+        //missing elements are found when nums[i] != i+1
         return (findnotEqual(nums));
     }
     
    static int[] func_disappearedElements(int[] arr){
         int i = 0;
        while (i < arr.length){
+           
            if(arr[i]==(i+1)){
+               //if element at index == number considering base 1 array.
                i++;
            }else {
                if(arr[i] == arr[arr[i]-1]){
+                  // if element is equal at ith and current position increment
                    i++;
                }else {
+                   // correct index of current element is stored in variable as it may //change when elements are swapped
                    int m = arr[i]-1;
                    int temp = arr[i];
                    arr[i] = arr[m];
