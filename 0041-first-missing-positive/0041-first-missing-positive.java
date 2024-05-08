68b89a62e290df1b86122a46ea549c6713f764da
ChinmayiPrakash <83453211+ChinmayiPrakash@users.noreply.github.com>
2024-05-08 15:53:48 -0400

    Time: 3 ms (33.05%), Space: 57.3 MB (24.14%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int firstMissingPositive(int[] arr) {
+            arr = cyclicSort(arr);
+        return ((FirstMissingPositive(arr)));
+
+    }
+    static int[] cyclicSort(int[] arr){
+        int i = 0;
+        int n = arr.length;
+        while(i<n){
+            if(arr[i]<=0 || arr[i]>n){
+                i++;
+            }
+            else {
+                if(arr[i]==i+1){
+                    i++;
+                }
+                else {
+                    int m = arr[i]-1;
+                    if(arr[i] == arr[m]){
+                        i++;
+                    }
+                    else {
+                        int temp = arr[i];
+                        arr[i] = arr[m];
+                        arr[m] = temp;
+                    }
+                }
+            }
+        }
+        return arr;
+    }
+    static int FirstMissingPositive(int[] arr) {
+        int val = 0;
+        for (int i = 0; i <= arr.length-1; i++) {
+            if(arr[i] != i+1){
+                return (i+1);
+            }
+            else{
+                val = arr[i]+1;
+            }
+
+        }
+        return val;
+    }
+}
