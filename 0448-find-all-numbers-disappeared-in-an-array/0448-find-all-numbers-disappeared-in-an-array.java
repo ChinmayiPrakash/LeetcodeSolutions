0f1c539347838911031b03d764ff156efd7f85c8
ChinmayiPrakash <83453211+ChinmayiPrakash@users.noreply.github.com>
2024-05-08 14:44:29 -0400

    Time: 8 ms (29.78%), Space: 53 MB (96.20%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+       nums = func_disappearedElements(nums);
+        return (findnotEqual(nums));
+    }
+    
+   static int[] func_disappearedElements(int[] arr){
+        int i = 0;
+       while (i < arr.length){
+           if(arr[i]==(i+1)){
+               i++;
+           }else {
+               if(arr[i] == arr[arr[i]-1]){
+                   i++;
+               }else {
+                   int m = arr[i]-1;
+                   int temp = arr[i];
+                   arr[i] = arr[m];
+                   arr[m ] = temp;
+               }
+           }
+       }
+       return arr;
+   }
+
+   static ArrayList<Integer> findnotEqual(int[] arr){
+        ArrayList<Integer> list = new ArrayList<>(10);
+       for (int i = 0; i < arr.length; i++) {
+           if(arr[i] != i+1){
+               list.add(i+1);
+           }
+
+       }
+       return list;
+   }
+}
