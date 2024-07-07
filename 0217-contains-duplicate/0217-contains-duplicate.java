d166c4a9a317b82a324eb930f71b799329560c77
ChinmayiPrakash <83453211+ChinmayiPrakash@users.noreply.github.com>
2024-07-07 13:17:24 -0400

    Time: 36 ms (5.19%), Space: 58.5 MB (44.92%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> fmap = new HashMap<Integer,Integer>();
+        //Adding all data to HashMap
+        for(int num:nums){
+            fmap.put(num,fmap.getOrDefault(num,0)+1);
+        }
+        //return true if value of any key is geater than 1
+        for(Map.Entry<Integer,Integer> entry : fmap.entrySet()){
+            if(entry.getValue()>1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
