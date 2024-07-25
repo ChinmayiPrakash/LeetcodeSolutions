b98b4563f34677f653c8f4feec24239b95c4ad3d
ChinmayiPrakash <83453211+ChinmayiPrakash@users.noreply.github.com>
2024-07-25 19:28:14 -0400

    Time: 5 ms (95.08%), Space: 42.3 MB (92.16%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        HashMap<Character,Integer> smap = new HashMap<>();
+      /*  HashMap<Character,Integer> smap = new HashMap<>();
         HashMap<Character,Integer> wmap = new HashMap<>();
         for (int i = 0; i < s1.length(); i++) {
             smap.put(s1.charAt(i),smap.getOrDefault(s1.charAt(i),0)+1);
@@ -25,6 +25,34 @@ class Solution {
         if(smap.equals(wmap)){
                     return true;
                 }
+        return false;*/
+        int n1 = s1.length();
+        int n2 = s2.length();
+        
+        if (n1 > n2) {  
+            return false;
+        }
+        
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for (int i = 0; i < n1; i++) {
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(freq1, freq2)) {
+            return true;
+        }
+        
+        int l = 0, r = n1;
+        while (r < n2) {
+            freq2[s2.charAt(r++) - 'a']++;  
+            freq2[s2.charAt(l++) - 'a']--;  
+            if (Arrays.equals(freq1, freq2)) {
+                return true;    
+            }
+        }
+        
         return false;
 
     }
