94ba74fd8f93dfdc5bda10116ffc9e7be31acab5
ChinmayiPrakash <83453211+ChinmayiPrakash@users.noreply.github.com>
2024-08-22 16:04:08 -0400

    Time: 1 ms (99.90%), Space: 43.4 MB (63.32%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -6,23 +6,14 @@ class Solution {
         return res;
     }
     static void helper(int index, ArrayList<Integer> elist, int[] nums,List<List<Integer>> res){
-        if(index== nums.length){
-            ArrayList<Integer> temp = new ArrayList<>(elist);
-            //Collections.sort(temp);
-            if(!res.contains(temp)){
-                res.add(temp );
-                //return;
-            }
-            return;
+       res.add(new ArrayList<>(elist) );
+        for (int i = index; i < nums.length; i++) {
+            if(i!=index && nums[i]==nums[i-1]) continue;
 
-        }
-        //for (int i = index; i < nums.length; i++) {
-            //if(i>index && nums[i]==nums[i-1]) break;
-
-            elist.add(nums[index]);
-            helper(index+1,elist,nums,res);
+            elist.add(nums[i]);
+            helper(i+1,elist,nums,res);
             elist.remove(elist.size()-1);
-            helper(index+1,elist,nums,res);
-       // }
+            //helper(index+1,elist,nums,res);
+        }
     }
 }
