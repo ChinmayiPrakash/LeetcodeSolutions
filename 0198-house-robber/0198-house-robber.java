d5887aecb484b6d9ee0f53de183791ca95b85901
ChinmayiPrakash <83453211+ChinmayiPrakash@users.noreply.github.com>
2024-10-13 20:21:32 -0400

    Time: 0 ms (100%), Space: 40.6 MB (88.45%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -7,15 +7,17 @@ class Solution {
             return Math.max(nums[0],nums[1]);
         }
 
-        int[] dp = new int[nums.length];
-        dp[0]=nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
+       // int[] dp = new int[nums.length];
+        //dp[0]=nums[0];
+        //dp[1] = Math.max(nums[0],nums[1]);
+
+        nums[1] = Math.max(nums[0],nums[1]); 
         
 
-        for(int i = 2; i<dp.length;i++){
-            dp[i]= Math.max((nums[i]+dp[i-2]),(dp[i-1]));
+        for(int i = 2; i<nums.length;i++){
+            nums[i]= Math.max((nums[i]+nums[i-2]),(nums[i-1]));
         }
-       return dp[nums.length - 1];
+       return nums[nums.length - 1];
     }
 
     
