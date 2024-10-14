9ee9cd0438bb508c2a1cc9cfa68b7c6caecb3e9d
ChinmayiPrakash <83453211+ChinmayiPrakash@users.noreply.github.com>
2024-10-13 20:26:00 -0400

    Time: 0 ms (100%), Space: 41 MB (36.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,4 +1,8 @@
 class Solution {
+    /*
+    TC = O(N) = 100.00%
+    SC = O(N) = 63.11%
+
     public int rob(int[] nums) {
         if(nums.length == 1){
             return nums[0];
@@ -16,6 +20,22 @@ class Solution {
             dp[i]= Math.max((nums[i]+dp[i-2]),(dp[i-1]));
         }
        return dp[nums.length - 1];
+    }*/
+    // we can reduce space complexity to O(1) by reassigning dp values to same array.
+
+    public int rob(int[] nums){
+        if(nums.length == 1){
+            return nums[0];
+        }
+        if(nums.length == 2){
+            return Math.max(nums[0],nums[1]);
+        }
+        nums[1] = Math.max(nums[0],nums[1]);
+        for(int i = 2 ; i<nums.length ; i++){
+            nums[i] = Math.max((nums[i]+nums[i-2]),(nums[i-1]));
+        }
+        return nums[nums.length - 1];
+
     }
 
     
