fb0ff064860753c045b58500f256f080fb74bc27
ChinmayiPrakash <83453211+ChinmayiPrakash@users.noreply.github.com>
2025-03-06 19:53:34 -0500

    Time: 1 ms (75.73%), Space: 45.7 MB (65.15%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,51 +1,4 @@
-import java.util.*;
-
 class Solution {
-    class Data {  // Capitalized class name (convention)
-        public int row;
-        public int col;
-
-        Data(int row, int col) {  // Removed empty constructor (not needed)
-            this.row = row;
-            this.col = col;
-        }
-    }
-
-    public void setZeroes(int[][] matrix) {
-        Queue<Data> values = new LinkedList<>();
-
-        // Step 1: Store positions of zeroes
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] == 0) {
-                    values.offer(new Data(i, j));
-                }
-            }
-        }
-
-        // Step 2: Set rows and columns to zero
-        while (!values.isEmpty()) {
-            Data data1 = values.poll();
-            int row = data1.row;
-            int col = data1.col;
-
-            // Fix boundary check: should be '<' instead of '<='
-            if (col < matrix[0].length) {
-                for (int i = 0; i < matrix.length; i++) {
-                    matrix[i][col] = 0;
-                }
-            }
-
-            if (row < matrix.length) {
-                for (int i = 0; i < matrix[0].length; i++) {
-                    matrix[row][i] = 0;
-                }
-            }
-        }
-    }
-}
-
-/*class Solution {
     class data{
         public int row;
         public int col;
@@ -69,16 +22,16 @@ class Solution {
         while(!values.isEmpty()){
            data data1 = new data();
            data1 = values.poll();
-           int col = data1.row;
-           int row = data1.col;
-           if(col < matrix.length){
+           int row = data1.row;
+           int col = data1.col;
+           if(col < matrix[0].length){
            for(int i =0;i<matrix.length;i++){
                 matrix[i][col]=0;
            }}
-            if(row<matrix[0].length){
+            if(row<matrix.length){
            for(int i =0;i<matrix[0].length;i++){
                 matrix[row][i]=0;
            }}
         }
     }
-}*/
+}
